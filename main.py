6ba0ced64cebe9ff4bf8f4af659b4a979933901f
kayhangavahi <48227209+kayhangavahi@users.noreply.github.com>
2020-04-25 18:18:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ Created on Sun Jan 26 14:56:35 2020
 
 @author: 16785
 """
-
 import MODIS as ds
+start_date = '2018-01-01'
+end_date   = '2019-01-01'
+product    = 'MCD12Q1'
 
+ds.dl_MODIS('<earthdata_username>','<earthdata_pass>', start_date, end_date, product)
 
-
-ds.dl_MODIS('<earthdata_username>','<earthdata_pass>','2018-01-01','2018-01-01','MCD12Q1')
