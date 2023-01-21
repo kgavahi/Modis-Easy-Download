8dd7a201de34b2306e93caaa410df8d41b104a9f
Keyhan Gavahi <kgavahi@uahpc.ua.edu>
2023-01-21 10:47:40 -0600

    Test run

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ Created on Sun Jan 26 14:56:35 2020
 """
 import MODIS as ds
 start_date = '2018-01-01'
-end_date   = '2019-01-01'
+end_date   = '2018-01-01'
 product    = 'MCD12Q1'
 
-ds.dl_MODIS('<earthdata_username>','<earthdata_pass>', start_date, end_date, product)
+ds.dl_MODIS('kgavahi194','ip2jTPU3yWlQ', start_date, end_date, product)
 
